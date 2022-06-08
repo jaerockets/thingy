f080e8cea3361b326eb834f5c656104fe6390f15
jaerockets <77167200+jaerockets@users.noreply.github.com>
2022-06-08 13:37:25 -0700

    f

--- reader.js
+++ reader.js
@@ -2,7 +2,7 @@ function list() {
     i=1
     swaglord=''
     while (i<=68) {
-        swaglord += `<img src=https://cdn.digital.baseballdigest.com/14d77c5e-7118-4975-878a-8bc4eded793f/484ca1d3-02c4-47bc-9c43-ad26e7dc9db2/large/${i}.jpg>\n`
+        swaglord += `<img src=https://cdn.digital.baseballdigest.com/14d77c5e-7118-4975-878a-8bc4eded793f/14538749-9041-4b28-a65e-449b49c8129a/large/${i}.jpg>\n`
         i+=1
     }
 }
